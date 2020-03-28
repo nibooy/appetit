084671418db9e8b8d810da4575c5447ce3550d3a
sk531 <sk531@duke.edu>
2020-03-28 10:41:56 -0700

    added user classes to backend

---
+++ appetit/appetit/Backend API/UserInfoAPI.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoAPI.swift
+//  appetit
+//
+//  Created by Mark Kang on 3/28/20.
+//  Copyright © 2020 Mark Kang. All rights reserved.
+//
+
+import Foundation
+
+class UserInfoAPI{
+    
+    func isValidUser(username:String, password:String)-> Bool{
+        return true
+    }
+}

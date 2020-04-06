ce9ac065cf63bc5d444bc9fdd76e78a1f526dddb
fwh5 <frankhu19@gmail.com>
2020-04-06 18:13:33 -0400

    added wireframe to storyboard

---
+++ appetit/appetit/Frontend Logic/RecipesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipesViewController.swift
+//  appetit
+//
+//  Created by Frank Hu on 4/6/20.
+//  Copyright © 2020 Mark Kang. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    @IBOutlet weak var rejectButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var recipeImage: UIImageView!
+    
+    @IBOutlet weak var atGlanceInfo: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

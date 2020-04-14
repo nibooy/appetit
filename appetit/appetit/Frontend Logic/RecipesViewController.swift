4b5253f4b0439114b3c5e8aef30e29c0ea767ade
sk531 <sk531@duke.edu>
2020-04-14 15:29:15 -0700

    added recipeapi details and documentation into viewcontroller class

--- appetit/appetit/Frontend Logic/RecipesViewController.swift
+++ appetit/appetit/Frontend Logic/RecipesViewController.swift
@@ -15,11 +15,38 @@ class RecipesViewController: UIViewController {
     @IBOutlet weak var recipeImage: UIImageView!
     
     @IBOutlet weak var atGlanceInfo: UIView!
+    let urlString = "https://api.edamam.com/search?"
+    let apiKey = "e789925699272fcebc9ebbc5957d99b1"
+    let appId = "798efc6d"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let mySession = URLSession(configuration: URLSessionConfiguration.default)
+                let urlWithQueryParameters = urlString
+                let url = URL(string: urlWithQueryParameters.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
+                let task = mySession.dataTask(with: url) { data, response, error in
+                
+                    guard error == nil else {
+                        //error message here for internet problems
+                        return
+                    }
+                    guard let jsonData = data else {
+                        print("No data")
+                        return
+                    }
+                    let decoder = JSONDecoder()
+                    do {
+//                        let result = try decoder.decode(//struct here, from: jsonData)
+//                        
+                        DispatchQueue.main.async {
+                            //reload data here
+                        }
+                    } catch {
+                        // error message here while loading data
+                    }
+                }
+                task.resume()
     }
     
     override func viewDidDisappear(_ animated: Bool) {

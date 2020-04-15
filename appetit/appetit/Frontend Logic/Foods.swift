eb4bf0143f27f0dd92a8d0f81b6828bc5dd413ac
Yoo Bin Shin <ys230@duke.edu>
2020-04-15 14:05:29 +0900

    JSON Serialization added

---
+++ appetit/appetit/Frontend Logic/Foods.swift
@@ -0,0 +1,14 @@
+//
+//  Foods.swift
+//  appetit
+//
+//  Created by Yoo Bin Shin on 4/15/20.
+//  Copyright © 2020 Mark Kang. All rights reserved.
+//
+
+import Foundation
+
+struct Foods {
+    var food_name: String = ""
+    var serving_qty: Int = 0
+}

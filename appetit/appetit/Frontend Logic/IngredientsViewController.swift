0b956b00fcabfb8f97f72f6d9541b7e5c9890b9f
fwh5 <frankhu19@gmail.com>
2020-04-17 01:00:11 -0400

    um

--- appetit/appetit/Frontend Logic/IngredientsViewController.swift
+++ appetit/appetit/Frontend Logic/IngredientsViewController.swift
@@ -8,26 +8,92 @@
 
 import UIKit
 
-class IngredientsViewController: UIViewController {
+private let reuseIdentifier = "FridgeCustomCell"
 
-    @IBOutlet weak var ingredientsView: UIView!
-    @IBOutlet weak var generateRecipe: UIButton!
+class IngredientsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var buttonView: UIView!
+    var fridge = [Food]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Connect to backend change the setup function
+        setup(n:10)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.alwaysBounceVertical = true
+        collectionView!.register(UINib.init(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        
+        
+        //code to add button to right
+        let menu = UIButton()
+        menu.imageView?.contentMode = .scaleAspectFit
+        menu.addTarget(self, action: #selector(menuButtonClicked), for: .touchUpInside)
+        menu.setImage(UIImage(imageLiteralResourceName: "menu"), for: .normal)
+        menu.widthAnchor.constraint(equalToConstant: 34.0).isActive = true
+        menu.heightAnchor.constraint(equalToConstant: 34.0).isActive = true
+        let rightItem = UIBarButtonItem(customView: menu)
+        
+        setupLeftTitle(title: "Generate Recipes")
+        
+        self.navigationItem.rightBarButtonItem  = rightItem
+        print(fridge)
+          
+    }
+    // MARK: - Navigation
+    //
+    //    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        // Get the new view controller using segue.destination.
+    //        // Pass the selected object to the new view controller.
+    //    }
+    
+    
+    // MARK: View Layout Setup
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    private let itemsPerRow: CGFloat = 2
+    
+    
+    // MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fridge.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FridgeCustomCell
+        cell.configureData(with: fridge[indexPath.row])
+        return cell
     }
-    */
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("User tapped on item \(indexPath.row)")
+    }
+    
+    //change function to however u like but make sure to keep last two lines- responsible for how we get add button
+    func setup(n: Int){
+        
+        for _ in 0...n-1{
+            let ingredient = Food(name: "Avocado", measurement: "2 Serving", image: #imageLiteral(resourceName: "Avocado"))
+            self.fridge.append(ingredient)
+        }
+   
+    }
+    
+    func setupLeftTitle(title : String){
+        let longTitleLabel = UILabel()
+        longTitleLabel.text = "Virtual Fridge"
+        longTitleLabel.font = .boldSystemFont(ofSize: 18)
+        longTitleLabel.sizeToFit()
+        let leftItem = UIBarButtonItem(customView: longTitleLabel)
+        self.navigationItem.leftBarButtonItem = leftItem
+    }
+    
+    @objc func menuButtonClicked(_ sender: UIButton) {
+        
+    }
 }
+

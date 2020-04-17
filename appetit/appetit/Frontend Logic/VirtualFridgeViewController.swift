39263124abe6f84d509e344aa0695d77144ebb31
fwh5 <frankhu19@gmail.com>
2020-04-17 00:15:52 -0400

    navbar

--- appetit/appetit/Frontend Logic/VirtualFridgeViewController.swift
+++ appetit/appetit/Frontend Logic/VirtualFridgeViewController.swift
@@ -12,10 +12,8 @@ private let reuseIdentifier = "FridgeCustomCell"
 
 class VirtualFridgeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var header: UIView!
-    @IBOutlet weak var menuButton: UIButton!
+    //@IBOutlet weak var menuButton: UIButton!
     
     // This is the list to hold our food objects. Backend - might need to add a field for an image to coredata.
     var fridge = [Food]()
@@ -29,19 +27,34 @@ class VirtualFridgeViewController: UIViewController, UICollectionViewDelegate, U
         collectionView.alwaysBounceVertical = true
         collectionView!.register(UINib.init(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
         collectionView!.register(UINib.init(nibName: "AddCell", bundle: nil), forCellWithReuseIdentifier: "AddCell")
-        menuButton.setImage(UIImage(imageLiteralResourceName: "menu"), for: .normal)
-        self.titleLabel.text = "Virtual Fridge"
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
+        //code to add button to right
+        let menu = UIButton()
+        menu.imageView?.contentMode = .scaleAspectFit
+        menu.addTarget(self, action: #selector(menuButtonClicked), for: .touchUpInside)
+        menu.setImage(UIImage(imageLiteralResourceName: "menu"), for: .normal)
+        menu.widthAnchor.constraint(equalToConstant: 34.0).isActive = true
+        menu.heightAnchor.constraint(equalToConstant: 34.0).isActive = true
+        let rightItem = UIBarButtonItem(customView: menu)
+        
+        setupLeftTitle(title: "Virtual Fridge")
+        
+        self.navigationItem.rightBarButtonItem  = rightItem
+        
+
+
     }
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
 
     
     // MARK: View Layout Setup
@@ -88,6 +101,15 @@ class VirtualFridgeViewController: UIViewController, UICollectionViewDelegate, U
         
     }
     
+    func setupLeftTitle(title : String){
+        let longTitleLabel = UILabel()
+        longTitleLabel.text = "Virtual Fridge"
+        longTitleLabel.font = .boldSystemFont(ofSize: 18)
+        longTitleLabel.sizeToFit()
+        let leftItem = UIBarButtonItem(customView: longTitleLabel)
+        self.navigationItem.leftBarButtonItem = leftItem
+    }
+    
     @objc func keyboardWillShow(notification: NSNotification) {
            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
                if self.view.frame.origin.y == 0 {
@@ -96,21 +118,31 @@ class VirtualFridgeViewController: UIViewController, UICollectionViewDelegate, U
            }
        }
 
-       @objc func keyboardWillHide(notification: NSNotification) {
+    @objc func keyboardWillHide(notification: NSNotification) {
            if self.view.frame.origin.y != 0 {
                self.view.frame.origin.y = 0
            }
        }
+    @objc func menuButtonClicked(_ sender: UIButton) {
+        
+    }
 }
 
 extension VirtualFridgeViewController: AddCellDelegate {
     func didAddPressButton() {
         print("add button was pressed")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: "scanningVC")
-        self.definesPresentationContext = true
-        controller.modalPresentationStyle = .overCurrentContext
-        self.present(controller, animated: true, completion: nil)
+        if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "scanningVC") as? AddingViewController {
+            //viewController.newsObj = newsObj
+            if let navigator = navigationController {
+                navigator.pushViewController(viewController, animated: true)
+            }
+        }
+     
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let controller = storyboard.instantiateViewController(withIdentifier: "scanningVC")
+//        self.definesPresentationContext = true
+//        controller.modalPresentationStyle = .overCurrentContext
+//        self.present(controller, animated: true, completion: nil)
     }
 
 }
